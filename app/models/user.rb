5bbc42ea49aa0e276e4f3bb60fb8070cda79c58b
Harasaki Yoshika <nya.nya.yoska@gmail.com>
2017-06-03 05:21:35 +0000

    add sign sign up and user detail

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+    
+    before_save { self.email = self.email.downcase }
+    validates :name, presence: true, length: {maximum: 50 }
+    VALID_RMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: {maximum: 255},
+    format: {with: VALID_RMAIL_REGEX},
+    uniqueness: {case_sensitive: false}
+    
+    has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
   root to: 'static_pages#home'
+  
+  get 'signup', to: 'users#new'
+  
+  resources :users
 end
