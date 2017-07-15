58c9ed239c556d084e51ceb0cc30f73a246ef7fb
Harasaki Yoshika <nya.nya.yoska@gmail.com>
2017-07-15 19:43:14 +0000

    チェンジ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,15 +7,17 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+       #アカウント作成時にuser_paramsを受け取りemergencyモデルを作成してインスタンスに代入
+      @emergency= Emergency.new(name: user_params[:name],email: user_params[:email] )
+      @emergency.save
+      
       flash[:success] = "Welcome to the Laplusmembers!"
       redirect_to @user
     else
       render 'new'
     end
     
-    #アカウント作成時にuser_paramsを受け取りemergencyモデルを作成してインスタンスに代入
-    @emergency= Emergency.new(name: user_params[:name],email: user_params[:email] )
-    @emergency.save
+   
   end
   
   def show
